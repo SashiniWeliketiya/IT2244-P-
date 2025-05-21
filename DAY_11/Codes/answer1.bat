2a51e88535a5d6786ef5a983dc2b3535cd0d5653
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-05-21 21:30:58 +0530

    Add files via upload

---
+++ DAY_11/Codes/answer1.bat
@@ -0,0 +1,33 @@
+cd %USERPROFILE%\Desktop
+mkdir CSC2244 Marks Exam
+cd CSC2244
+mkdir practical theory "exam papers"
+cd practical
+echo.> practical.txt
+echo.> practical.docx
+echo.> practical.pptx
+cd ..
+
+cd theory
+echo.> theory.txt
+echo.> theory.docx
+echo.> theory.pptx
+cd ..
+
+cd "exam papers"
+echo.> exam.txt
+echo.> exam.docx
+echo.> exam.pptx
+cd ..
+
+cd %USERPROFILE%\Desktop
+echo.> "Icae Marks.xlsx"
+echo.> "Final Exam Marks.xlsx"
+move "Icae Marks.xlsx" Marks
+move "Final Exam Marks.xlsx" Marks
+
+xcopy /E /I Marks Exam\Marks
+
+attrib +h Exam
+
+
